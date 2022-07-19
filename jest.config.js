e109235542793cc8b95a9c7b8f17e16f7c103cb1
radek79 <radek79@gmail.com>
2022-07-18 17:41:21 -0700

    got getElementById working with jsDom

--- jest.config.js
+++ jest.config.js
@@ -2,17 +2,14 @@
 /** @type {import('@jest/types').Config.InitialOptions} */
 const config = {
 	verbose: true,
+	displayName: {
+		name: 'RODSTEST',
+		color: 'blue',
+	},
+	testEnvironment: 'jsdom',
+	testEnvironmentOptions: {
+		html: "<p id=\"root\"></p>"
+    }
 };
 
 module.exports = config;
-
-// Or async function
-module.exports = async () => {
-	return {
-		verbose: true,
-		displayName: {
-			name: 'RODSTEST',
-			color: 'blue',
-		},
-	};
-};
--- myapex.test.js
+++ myapex.test.js
@@ -2,7 +2,22 @@
  * @jest-environment jsdom
  */
 
-test('use jsdom in this test file', () => {
-	const element = document.createElement('div');
-	expect(element).not.toBeNull();
+test('use jsdom in a test file', () => {
+	const element = global.document.createElement('div');
+	element.id = 'myid';
+	
+	const root = global.document.getElementById('root');
+	root.appendChild(element);
+
+
+	const mychart = global.document.getElementById("myid");
+	//const other = global.document.getElementsByTagName('p');
+
+	console.log('************');
+	console.log(`Root is ${root}`);
+	console.log(`Object is  ${mychart}`);
+	console.log(`Length is  ${document.getElementsByTagName('*').length}`);
+	//expect(mychart).toHaveLength(1);
+	//expect(mychart.length).toBeGreaterThan(0);
+	expect(mychart).not.toBeNull();
 });
